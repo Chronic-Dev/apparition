e6a018dd8a961e8a8640e555091263df8ebc0adc
Nicolas Haunold <westbaer@gmail.com>
2011-05-06 01:23:39 +0200

    added mbdb and mbdx structures. contains most likely errors, havent tested it.

--- src/mbdb.h
+++ src/mbdb.h
@@ -1,12 +1,39 @@
-/*
- * mbdb.h
- *
- *  Created on: May 5, 2011
- *      Author: posixninja
- */
+#ifndef MBDB_H
+#define MBDB_H
 
-#ifndef MBDB_H_
-#define MBDB_H_
+#include <unistd.h>
+#include <time.h>
 
+struct mbdb_property {
+    char name[];
+    char value[];
+    struct mbdb_property *next;
+};
 
-#endif /* MBDB_H_ */
+struct mbdb_record {
+    char domain[];
+    char path[];
+    char target[];	      // absolute path
+    char datahash[];	      // SHA1 hash
+    char unknown1[];
+    uint16_t mode;	      // Axxx = symlink, 4xxx = dir, 8xxx = file
+    uint32_t unknown2;
+    uint32_t unknown3;
+    uint32_t uid;
+    uint32_t gid;
+    uint32_t time1;
+    uint32_t time2;
+    uint32_t time3;
+    uint64_t length;	      // 0 if link or dir
+    uint8_t flag;	      // 0 if link or dir
+    uint8_t properties;	      // number of properties
+    struct mbdb_property *property;
+};
+
+struct mbdb_file {
+    uint8_t magic[6];		    // 'mbdb\5\0'
+    struct mbdb_record *record;
+};
+
+#endif
+
--- src/mbdx.h
+++ src/mbdx.h
@@ -1,12 +1,21 @@
-/*
- * mbdx.h
- *
- *  Created on: May 5, 2011
- *      Author: posixninja
- */
+#ifndef MBDX_H
+#define MBDX_H
 
-#ifndef MBDX_H_
-#define MBDX_H_
+#include <unistd.h>
 
+struct mbdx_record {
+    uint8_t key[20];		  // key / filename
+    uint32_t offset;		  // offset of the mbdb file (+ 7 bytes)
+    uint16_t mode;		  // filemode: Axxx = symlink, 4xxx = dir, 8xxx = file
+    
+    struct mbdx_record *next;
+};
 
-#endif /* MBDX_H_ */
+struct mbdx_file {
+    uint8_t magic[4];		  // "mbdx\2\0"
+    uint32_t count;		  // count
+    struct mbdx_record *record;    
+};
+
+#endif
+
