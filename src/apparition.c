b3e931739979c69921709ac3fee6287135d06bd9
Joshua Hill <posixninja@gmail.com>
2011-05-05 11:29:19 -0400

    initial commit

---
+++ src/apparition.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : apparition.c
+ Author      : Chronic-Dev Team
+ Version     :
+ Copyright   : (c) 2011 Chronic-Dev, LLC
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World"); /* prints Hello World */
+	return EXIT_SUCCESS;
+}
