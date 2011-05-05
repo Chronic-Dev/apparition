df1f070c177d5cfd4a87fc5b3a0be2ecbcbf8743
Joshua Hill <posixninja@gmail.com>
2011-05-05 14:37:12 -0400

    Added arch info to other 2 main iles, and added myself to authors. if you do any work at all please add your name to the AUTHORS file

--- src/apparition.c
+++ src/apparition.c
@@ -9,9 +9,9 @@
  */
 
 /* This is not only the entry point, but since this is a console application
- * this is also the applications View. No other file in this project is allowed
- * to print to stdout other then this file. You may print to stderr in the case
- * of a non-recoverable error, or for debugging purposes (must be removed for release)
+ *  this is also the applications View. No other file in this project is allowed
+ *  to print to stdout other then this file. You may print to stderr in the case
+ *  of a non-recoverable error, or for debugging purposes (must be removed for release)
  */
 
 #include <stdio.h>
--- src/backup.c
+++ src/backup.c
@@ -5,3 +5,8 @@
  *      Author: posixninja
  */
 
+/* This is the application Model
+ *  Functions in this file should handle openning, reading, and parsing
+ *  backup directories. it should allow access to mbdb and mbdx, as well
+ *  as extract and injecting files into the databases.
+ */
