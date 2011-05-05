cdf0039016f8c73b509680829e9e6e2b0f9ed2d2
Joshua Hill <posixninja@gmail.com>
2011-05-05 13:56:52 -0400

    updated device architechural info

--- src/device.c
+++ src/device.c
@@ -5,3 +5,9 @@
  *      Author: posixninja
  */
 
+/* This is the application Controller.
+ *  This file should contain functions to control the device
+ *  through lockdownd services. This file acts as a proxy to
+ *  the afc, mb2, and nos services and should handle messages.
+ */
+
