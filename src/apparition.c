cc77dd89ebf1d788689bd110bc4ee8f25f07a025
Joshua Hill <posixninja@gmail.com>
2011-05-05 20:39:14 -0400

    filled out main function in apparition.c a little more

--- src/apparition.c
+++ src/apparition.c
@@ -18,6 +18,7 @@
 #include <stdlib.h>
 
 #include "backup.h"
+#include "device.h"
 
 int main(void) {
 	int err = 0;
@@ -29,12 +30,67 @@ int main(void) {
 		return -1;
 	}
 
+	// Append a file to the current backup object
 	err = backup_add_file(backup, "./whatever", "/tmp/whatever");
 	if(err < 0) {
 		printf("Unable to add file to backup\n");
 		backup_free(backup);
 	}
 
-	backup_free(backup);
+	// Pass a UUID here if you want to target a single device,
+	//  or NULL to select the first one it finds
+	device_t* device = device_open(NULL);
+	if(device == NULL) {
+		printf("Unable to find a device to use\n");
+		backup_free(backup);
+		return -1;
+	}
+
+	// Open and initialize the afc connection
+	afc_t* afc = afc_open(device);
+	if(afc == NULL) {
+		printf("Unable to open connection to afc service\n");
+		device_free(device);
+		backup_free(backup);
+		return -1;
+	}
+
+	// Send a file from your computer to the device
+	err = afc_send_file(afc, "local/file.txt", "remote/file.txt");
+	if(err < 0) {
+		printf("Unable to send file over apple file conduit\n");
+		afc_free(afc);
+		device_free(device);
+		backup_free(backup);
+		return -1;
+	}
+	// Close our connection, but don't free our object
+	afc_close(afc);
+
+	// Open and initialize the mb2 connection
+	mb2_t* mb2 = mb2_open(device);
+	if(mb2 == NULL) {
+		printf("Unable to open connection to mobilebackup2 service");
+		afc_free(afc);
+		device_free(device);
+		backup_free(backup);
+	}
+
+	// Perform a restore from a backup object
+	err = mb2_restore(mb2, backup);
+	if(err < 0) {
+		printf("Unable to restore our backup object\n");
+		mb2_free(mb2);
+		afc_free(afc);
+		device_free(device);
+		backup_free(backup);
+	}
+	mb2_close(mb2);
+
+	// If open, then close and free structures
+	if(mb2) mb2_free(mb2);
+	if(afc) afc_free(afc);
+	if(device) device_free(device);
+	if(backup) backup_free(backup);
 	return 0;
 }
