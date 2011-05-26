50a69ce9965bc14762b1fe5912f2789f7c9f0edb
Kevin Bradley <postmansyndrome@gmail.com>
2011-05-25 19:13:28 -0700

    updated crashreportcopy.c to add standard boilerplate code, still builds

--- src/crashreportcopy.c
+++ src/crashreportcopy.c
@@ -10,11 +10,36 @@
 
 
 crashreportcopy_t* crashreportcopy_open(lockdown_t* lockdown) {
-	return NULL;
+	int err = 0;
+	unsigned short port = 0;
+	crashreportcopy_t* copier = NULL;
+	
+	err = lockdown_start_service(lockdown, "com.apple.crashreportcopymobile", &port);
+	if(err < 0 ) {
+		return NULL;
+	}
+	
+	err = idevice_connect(lockdown->device, copier->port, copier->connection);
+	if(err < 0) {
+		return NULL;
+	}
+	idevice_disconnect(copier->connection);
+	return copier;
 }
 
 crashreportcopy_t* crashreportcopy_create(lockdown_t* lockdown) {
-	return NULL;
+	crashreportcopy_t* copier = NULL;
+	if(lockdown == NULL) {
+		printf("Unable to create crashreportercopier object\n");
+		return NULL;
+	}
+	
+	copier = (crashreportcopy_t*) malloc(sizeof(crashreportcopy_t));
+	if(copier == NULL) {
+		return NULL;
+	}
+	
+	return copier;
 }
 
 int crashreportcopy_close(crashreportcopy_t* copier) {
