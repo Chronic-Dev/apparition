b4feeeee0b177a50a1421ae65f6aa20feaf5e402
Joshua Hill <posixninja@gmail.com>
2011-07-14 20:55:26 -0400

    Updated copyright info and headers, if you worked on a file please add yourself into the credits now or forever hold your peace

--- src/apparition.c
+++ src/apparition.c
@@ -1,18 +1,21 @@
-/*
- ============================================================================
- Name        : apparition.c
- Author      : Chronic-Dev Team
- Version     :
- Copyright   : (c) 2011 Chronic-Dev, LLC
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
-/* This is not only the entry point, but since this is a console application
- *  this is also the applications View. No other file in this project is allowed
- *  to print to stdout other then this file. You may print to stderr in the case
- *  of a non-recoverable error, or for debugging purposes (must be removed for release)
- */
+/**
+  * GreenPois0n Apparition - apparition.h
+  * Copyright (C) 2010 Chronic-Dev Team
+  * Copyright (C) 2010 Joshua Hill
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
--- src/backup.c
+++ src/backup.c
@@ -1,15 +1,21 @@
-/*
- * backup.c
- *
- *  Created on: May 5, 2011
- *      Author: posixninja
- */
-
-/* This is the application Model
- *  Functions in this file should handle openning, reading, and parsing
- *  backup directories. it should allow access to mbdb and mbdx, as well
- *  as extract and injecting files into the databases.
- */
+/**
+  * GreenPois0n Apparition - backup.c
+  * Copyright (C) 2010 Chronic-Dev Team
+  * Copyright (C) 2010 Joshua Hill
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
 
 #include <stdio.h>
 #include <stdlib.h>
